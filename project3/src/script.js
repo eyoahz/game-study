e91592e8cc46fa09e0a80f37951aa2e178da96b3
zhaoye <zhaoyebaitu@gmail.com>
2024-12-03 15:41:47 +0800

    feat: 增加敌人动画和随机生成逻辑

--- project3/src/script.js
+++ project3/src/script.js
@@ -5,35 +5,48 @@ const canvas = document.querySelector('#canvas1');
 const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = canvas.width = 500;
 const CANVAS_HEIGHT = canvas.height = 1000;
-/*
-const enemy1 = {
-    x: 0,
-    y: 0,
-    width: 200,
-    height: 200,
-}*/
+const numberOfEnemies = 5;
+const enemiesArray = [];
+let gameFrame = 0;
 
 class Enemy {
-    constructor(x, y, width, height) {
+    constructor(x, y) {
+        this.image = new Image();
+        this.image.src = `assets/enemy1.png`
         this.x = x;
         this.y = y;
-        this.width = width;
-        this.height = height;
+        this.speed = Math.random() * 4 - 2;
+        this.spriteWidth = 293;
+        this.spriteHeight = 155;
+        this.width = this.spriteWidth / 2.5;
+        this.height = this.spriteHeight / 2.5;
+        this.frame = 0;
+        this.flapSpeed = Math.floor(Math.random() * 3 + 1);
     }
     update() {
-        this.x++;
-        this.y++;
-        ctx.fillRect(this.x, this.y, this.width, this.height)
+        this.x += this.speed;
+        this.y += this.speed;
+        if(gameFrame % this.flapSpeed === 0) {
+            this.frame > 4 ? this.frame = 0 : this.frame++;
+        }
+    }
+    draw() {
+        ctx.strokeRect(this.x, this.y, this.width, this.height)
+        ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height)
     }
 }
 
-const enemy1 = new Enemy(10, 50, 100, 100);
-const enemy2 = new Enemy(10, 850, 100, 100);
+for(let i = 0; i < numberOfEnemies; i++) {
+    enemiesArray.push(new Enemy(Math.random() * CANVAS_WIDTH, Math.random() * CANVAS_HEIGHT))
+}
 
 function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    enemy1.update();
-    ctx.fillRect(enemy1.x, enemy1.y, enemy1.width, enemy1.height);
+    enemiesArray.forEach(enemy => {
+        enemy.update();
+        enemy.draw();
+    })
+    gameFrame++
     requestAnimationFrame(animate);
 }
 animate()
